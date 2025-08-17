995f679d40cf92750149a8d5055be4f3d55164ae
keyhansa <keyhan.ahmad@gmail.com>
2025-08-17 21:48:34 +0330

    modified about

--- components/home/home_about_us/About_us.tsx
+++ components/home/home_about_us/About_us.tsx
@@ -4,10 +4,14 @@ import { useInView } from "react-intersection-observer";
 import { AnimatePresence, motion } from "framer-motion";
 import MaskText from "@/components/animation/MaskText";
 import TextMask from "@/components/animation/TextMask";
+import { useLanguage } from "@/contexts/LanguageContext";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 type Props = {};
 
 const About_us = (props: Props) => {
+  const { t, isRTL } = useLanguage();
   const [trigger, setTrigger] = useState<boolean>(false);
 
   useEffect(() => {
@@ -18,13 +22,6 @@ const About_us = (props: Props) => {
 
   const [ref, inView] = useInView();
   const [ref2, inView2] = useInView();
-
-  // console.log(trigger, "trigger");
-
-  // const {  inView, entry } = useInView({
-  //   threshold: 0,
-  //   triggerOnce: trigger ? false : true,
-  // });
 
   return (
     <AnimatePresence>
@@ -69,55 +66,37 @@ const About_us = (props: Props) => {
                     )}
                   </div>
                   <div ref={ref2} className="text-white_text">
-                    <p className="font-miracle scroll-m-20 text-2xl font-semibold tracking-[.10em]">
-                      High Quality{" "}
+                    <p className="scroll-m-20 text-2xl font-semibold tracking-[.10em]">
+                      {t("about.subtitle")}
                     </p>
-                    {/* <TextMask
-                      inview={inView2}
-                      text=""
-                      customStyles="w-[100%]  md:w-[80%] max-sm:w-[100%]  max-xl:w-[90%] leading-7 [&:not(:first-child)]: mt-3 lg:text-[17px] sm:text-[16px] max-sm:text-[14px] font-helvetica"
-                    >
-                      The five-star Bank Hotel was reopened to visitors in 2016.
-                      The building was renovated and modernized to meet the
-                      expectations of the most demanding guests. We offer
-                      luxurious rooms, numerous facilities, and exceptional
-                      service.
-                    </TextMask> */}
                     <MaskText
                       inview={inView2}
-                      customStyles="w-[100%]  md:w-[80%] max-sm:w-[100%]  max-xl:w-[90%] leading-7 [&:not(:first-child)]: mt-3 lg:text-[17px] sm:text-[16px] max-sm:text-[14px] font-helvetica"
+                      customStyles={`w-[100%]  md:w-[80%] max-sm:w-[100%]  max-xl:w-[90%] leading-7 [&:not(:first-child)]: mt-3 lg:text-[17px] sm:text-[16px] max-sm:text-[14px] font-${isRTL ? "" : "helvetica"} font-light`}
                     >
-                      The five-star Bank Hotel was reopened to visitors in 2016.
-                      The building was renovated and modernized to meet the
-                      expectations of the most demanding guests. We offer
-                      luxurious rooms, numerous facilities, and exceptional
-                      service.
+                      {t("about.description")}
                     </MaskText>
-                    {/* <p className="w-[100%]  md:w-[80%] max-sm:w-[100%]   max-xl:w-[90%] leading-7 [&:not(:first-child)]: mt-3 lg:text-[17px] sm:text-[16px] max-sm:text-[14px] font-helvetica">
-                      The five-star Bank Hotel was reopened to visitors in 2016.
-                      The building was renovated and modernized to meet the
-                      expectations of the most demanding guests. We offer
-                      luxurious rooms, numerous facilities, and exceptional
-                      service.{" "}
-                    </p> */}
+                  </div>
+                  <div className="w-full flex justify-end px-4">
+                    <Link href="/about">
+                      <Button>
+                        {t("about.cta")}
+                      </Button>
+                    </Link>
                   </div>
                 </div>
                 <div className="absolute top-[-10px] xl:left-[250px] lg:left-[220px] w-[100%] z-[-1px] ">
                   <img src="./grouped.webp" alt="img" />
                 </div>
-                {/* <div className="absolute top-[-20px] left-[-20px] w-[90%] z-[-1px] ">
-                <img src="./Ellipse.webp" alt="img" />
-              </div> */}
               </div>
               <div
                 id="things"
-                className="flex flex-col  font-miracle absolute z-10 top-1/3 max-[770px]:left-[50%]  md:left-[70%] lg:left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-[750px]:flex max-[750px]:flex-row  max-[750px]:gap-[25px] max-[750px]:top-[-10px] "
+                className="flex flex-col absolute z-10 top-1/3 max-[770px]:left-[50%]  md:left-[70%] lg:left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-[750px]:flex max-[750px]:flex-row  max-[750px]:gap-[25px] max-[750px]:top-[-10px] "
               >
-                <h1 className="min-[900px]:text-[80px] xl:text-[100px] sm:text-[70px] max-sm:text-[60px] max-[330px]:text-[50px]  scroll-m-20  font-extrabold tracking-[.10em]  text-golden_yellow">
-                  ABOUT
+                <h1 className="min-[900px]:text-[80px] xl:text-[100px] sm:text-[70px] max-sm:text-3xl max-[330px]:text-[50px]  scroll-m-20  font-bold tracking-[.10em]  text-golden_yellow">
+                  {t("about.title1")}
                 </h1>
-                <h1 className="lg:text-[100px] sm:text-[70px] max-sm:text-[60px]  text-end text-white_text scroll-m-20 font-extrabold tracking-[.10em]">
-                  US
+                <h1 className="lg:text-[100px] sm:text-[70px] max-sm:text-3xl  text-end text-white_text scroll-m-20 font-bold tracking-[.10em]">
+                  {t("about.title2")}
                 </h1>
               </div>
               <div className="relative w-[300px] min-w-[300px]  min-md:flex max-md:justify-end max-lg:hidden">
@@ -130,7 +109,6 @@ const About_us = (props: Props) => {
                     </ImageReveal>
                   )}
                 </div>
-                {/* <div className="absolute h-[100%] w-[1px] bg-[#3F5148] top-0 left-[-150px]"></div> */}
                 <div className="absolute w-[50px] top-[0px] left-[-45px]">
                   <img src="./label.webp" alt="img" />
                 </div>
@@ -138,11 +116,7 @@ const About_us = (props: Props) => {
                   <img src="./button-default.webp" alt="img" />
                 </div>
               </div>
-              {/* <div className="absolute w-[65%] top-[-20px] right-[0px]">
-              <img src="./Vector_14.webp" alt="img" />
-            </div> */}
             </div>
-            {/* <div className="absolute h-[100%] z-20 w-[1px] bg-[#3F5148] top-0 left-[-140px]"></div> */}
           </div>
         </div>
       </div>
