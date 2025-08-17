ed432fb706a4e3bd353ce55b26a5218c13d36b61
keyhansa <keyhan.ahmad@gmail.com>
2025-08-17 21:48:59 +0330

    modified

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-royal_green text-primary-foreground hover:bg-primary/90",
+        default: "bg-golden_yellow text-text_royal_green hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input border-golden_yellow bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input border-golden_yellow hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
