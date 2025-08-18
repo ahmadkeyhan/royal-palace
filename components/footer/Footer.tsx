ad1d815ae63eade7498e17ecf311e49510e452e6
keyhansa <keyhan.ahmad@gmail.com>
2025-08-18 14:29:25 +0330

    fixed footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,20 +1,22 @@
 import { usePathname } from "next/navigation";
 import React from "react";
 import { FaArrowRight } from "react-icons/fa6";
+import { useLanguage } from "@/contexts/LanguageContext";
 
 type Props = {};
 
 const Footer: React.FC = (props: Props) => {
+  const {t} = useLanguage()
   const route = usePathname();
 
   return (
     <footer
       className={`${route == "/facility" ? "bg-regal_green" : "bg-off-white"} ${
         route == "/facility" ? "text-white" : "#000"
-      }  border-[1px] border-transparent  font-helvetica`}
+      }  border-[1px] border-transparent font-ravi text-sm`}
     >
       <div className="min-[680px]:w-[95%] w-[95%] mx-auto mt-[50px] pb-[50px] flex items-start justify-start gap-[50px] flex-wrap ">
-        <div className="flex flex-col items-start gap-[10px] max-[480px]:hidden">
+        {/* <div className="flex flex-col items-start gap-[10px] max-[480px]:hidden">
           <p className="text-xl">ABOUT US</p>
           <p className="leading-6 font-helvetica w-[280px]">
             The five-star hotel in a beautiful European city with a modern
@@ -55,12 +57,11 @@ const Footer: React.FC = (props: Props) => {
           <p className="opacity-[0.4]  mt-6 font-helvetica">
             Designed by <a>Andrew Vynarchyk</a>{" "}
           </p>
-        </div>
-        <div className="w-full max-[480px]:block hidden">
+        </div> */}
+        <div className="w-full">
           <div className="h-[1px] w-full bg-gray-200 hidden "> </div>
           <p className="opacity-[0.4]  text-center mt-6">
-            {" "}
-            © 2021 All rights reserved. BankHotel{" "}
+            {t("footer.copyright")}
           </p>
         </div>
       </div>
