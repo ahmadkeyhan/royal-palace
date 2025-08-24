0e22bf8c419626a427f5e5ed1e2c518b0ef02e30
keyhansa <keyhan.ahmad@gmail.com>
2025-08-24 22:16:22 +0330

    widen cafe text

--- components/menu/menus.tsx
+++ components/menu/menus.tsx
@@ -7,7 +7,7 @@ export const Menus = () => {
     <div className="w-full bg-regal_green h-[calc(100vh-177px)] flex flex-col">
       <Link href="/menu/cafe" className="h-full w-full text-off-white bg-teal-700 flex flex-col gap-4 grow justify-center items-center">
         <h2 className="font-bold text-5xl">{isRTL? "منوی" : "CAFE"}</h2>
-        <h2 className="font-bold text-5xl">{isRTL? "کافه" : "MENU"}</h2>
+        <h2 className="font-bold text-5xl">{isRTL? "کافــــه" : "MENU"}</h2>
       </Link>
 
       <Link href="/menu/restaurant" className="h-full w-full flex flex-col gap-4 grow justify-center items-center text-golden_yellow">
