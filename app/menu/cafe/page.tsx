d4ab371935ec6366f2b4d91d88c04bcc61ad61e7
keyhansa <keyhan.ahmad@gmail.com>
2025-08-26 10:46:07 +0330

    used language and ui improve

--- app/menu/cafe/page.tsx
+++ app/menu/cafe/page.tsx
@@ -1,17 +1,19 @@
+"use client"
 
 import MenuCategories from "@/components/menu/menuCategories";
 import Navbar from "@/components/navbar/Navbar";
-import { Loader2, Snowflake, Hand,BookOpenText } from "lucide-react";
+import { useLanguage } from "@/contexts/LanguageContext";
 
-export default async function Home() {
 
+export default function CafeMenu() {
+  const {isRTL} = useLanguage()
   return (
-    <main className="min-h-screen bg-off-white">
-        <Navbar />
+    <main className="min-h-screen bg-teal-700">
+      <Navbar />
       <div className="container px-4 py-6 mx-auto max-w-3xl">
         <section className="space-y-4"> 
-          <div className="flex justify-center items-center text-qqteal gap-2 mb-2">
-            <h1 className="text-3xl font-bold text-teal-600">منوی کافه</h1>
+          <div className="flex justify-center items-center text-qqteal gap-2 mb-2 py-4">
+            <h1 className="text-4xl font-bold text-off-white">{isRTL? "منوی کافه" : "CAFE MENU"}</h1>
           </div>
           <MenuCategories />
           
