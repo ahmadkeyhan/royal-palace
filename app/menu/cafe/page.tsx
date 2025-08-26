54d3e2bd64ac5f26e4169d8bd793f0003aa46bce
keyhansa <keyhan.ahmad@gmail.com>
2025-08-26 12:35:16 +0330

    added vat text and footer to menu pages

--- app/menu/cafe/page.tsx
+++ app/menu/cafe/page.tsx
@@ -3,14 +3,14 @@
 import MenuCategories from "@/components/menu/menuCategories";
 import Navbar from "@/components/navbar/Navbar";
 import { useLanguage } from "@/contexts/LanguageContext";
-
+import Footer from "@/components/footer/Footer";
 
 export default function CafeMenu() {
   const {isRTL} = useLanguage()
   return (
-    <main className="min-h-screen bg-teal-700">
+    <main className="min-h-screen relative bg-teal-700">
       <Navbar />
-      <div className="container px-4 py-6 mx-auto max-w-3xl">
+      <div className="container min-h-screen px-4 py-6 mx-auto max-w-3xl">
         <section className="space-y-4"> 
           <div className="flex justify-center items-center text-qqteal gap-2 mb-2 py-4">
             <h1 className="text-4xl font-bold text-off-white">{isRTL? "منوی کافه" : "CAFE MENU"}</h1>
@@ -19,6 +19,8 @@ export default function CafeMenu() {
           
         </section>
       </div>
+      <div className="sticky py-4 px-12 w-full text-center bottom-0 bg-teal-700/70 backdrop-blur-sm text-off-white font-ravi text-sm">{isRTL? "به مبالغ فوق ۱۰ درصد مالیات بر ارزش افزوده اضافه خواهد شد." : "10% VAT will be added to the above prices."}</div>
+        <Footer />
     </main>
   );
 }
--- app/menu/restaurant/page.tsx
+++ app/menu/restaurant/page.tsx
@@ -3,14 +3,15 @@
 import MenuCategories from "@/components/menu/menuCategories";
 import Navbar from "@/components/navbar/Navbar";
 import { useLanguage } from "@/contexts/LanguageContext";
+import Footer from "@/components/footer/Footer";
 
 
 export default function RestaurantMenu() {
   const {isRTL} = useLanguage()
   return (
-    <main className="min-h-screen bg-regal_green">
+    <main className="min-h-screen relative bg-regal_green">
       <Navbar />
-      <div className="container px-4 py-6 mx-auto max-w-3xl">
+      <div className="container min-h-screen px-4 py-6 mx-auto max-w-3xl">
         <section className="space-y-4"> 
           <div className="flex justify-center items-center gap-2 mb-2 py-4">
             <h1 className="text-4xl font-bold text-golden_yellow">{isRTL? "منوی رستوران" : "RESTAURANT MENU"}</h1>
@@ -19,6 +20,8 @@ export default function RestaurantMenu() {
           
         </section>
       </div>
+      <div className="sticky py-4 px-12 w-full text-center bottom-0 bg-regal_green/70 backdrop-blur-sm text-off-white font-ravi text-sm">{isRTL? "به مبالغ فوق ۱۰ درصد مالیات بر ارزش افزوده اضافه خواهد شد." : "10% VAT will be added to the above prices."}</div>
+        <Footer />
     </main>
   );
 }
